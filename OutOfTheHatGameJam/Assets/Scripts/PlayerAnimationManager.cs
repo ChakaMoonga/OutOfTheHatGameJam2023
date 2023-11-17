be7c58413308ffd9c2d59c8e7f698f4a8f85c9ca
navinau <112662956+navinau@users.noreply.github.com>
2023-11-16 18:14:34 -0600

    help

--- OutOfTheHatGameJam/Assets/Scripts/PlayerAnimationManager.cs
+++ OutOfTheHatGameJam/Assets/Scripts/PlayerAnimationManager.cs
@@ -4,22 +4,59 @@ using UnityEngine;
 
 public class PlayerAnimationManager : MonoBehaviour
 {
-    Rigidbody2D rb;
-
+    public  Rigidbody2D rb;
+    public Transform squash;
     //handling animation
-    SpriteRenderer sprite;
-    Animator anim;
+    //public SpriteRenderer sprite1;
+    // public SpriteRenderer sprite2;
+  //  public SpriteRenderer sprite3;
+    private Animator anim;
+    private bool isMoving;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        //sprite1 = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+
     }
 
     private void FixedUpdate()
     {
-        anim.SetFloat("speed", rb.velocity.magnitude);
+       // anim.SetFloat("speed", rb.velocity.magnitude);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            squash.transform.Rotate(0, 180f, 0);
+           // sprite1.flipX = true;
+            //sprite2.flipX = true;
+            //sprite3.flipX = true;
+            isMoving = true;
+            anim.SetBool("isMoving", true);
+        }
+        else if( Input.GetKeyUp(KeyCode.A))
+        {
+            squash.transform.Rotate(0, 180f, 0);
+            isMoving = false;
+            anim.SetBool("isMoving",false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           // squash.transform.Rotate(0, 180f, 0);
+            //sprite1.flipX = false;
+            //sprite2.flipX = false;
+            //sprite3.flipX = false;
+            anim.SetBool("isMoving", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            //squash.transform.Rotate(0, 180f, 0);
+            isMoving = false;
+            anim.SetBool("isMoving", false);
+        }
     }
 }
