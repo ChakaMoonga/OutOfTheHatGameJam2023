fc422f429977c5dce888bc459095878ec09c29a3
KavadloD <112662875+KavadloD@users.noreply.github.com>
2023-11-17 18:09:15 -0600

    Tick Stuff

--- OutOfTheHatGameJam/Assets/Scripts/TickChecker.cs
+++ OutOfTheHatGameJam/Assets/Scripts/TickChecker.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class TickChecker : MonoBehaviour
 {
+    public Tick tickScript;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,7 @@ public class TickChecker : MonoBehaviour
     {
         if(col.CompareTag("Player"))
         {
-            print("HIT");
+            tickScript.TickFall();
         }
     }
 }
